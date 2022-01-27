ad656fb79bb79cff9a7709d242bd240e167b301f
vittosba <vitsbardella@gmail.com>
2022-01-27 20:01:07 +0100

    project layout and comics model and migration

---
+++ database/migrations/2022_01_27_185054_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->decimal('price', 4, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container flex-grow-1">
+        <h1>Welcome to Comics Archive</h1>
+    </div>
+@endsection
